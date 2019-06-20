03c9e923370c5ee098472e52375dd32aea6a595b
appu2312 <39756704+appu2312@users.noreply.github.com>
2019-06-20 12:41:26 +0530

    Removed print statements

--- BM_SparkJobs.py
+++ BM_SparkJobs.py
@@ -12,9 +12,9 @@ import pandas as pd
 webpage="http://hn1-devrsi.ksccfrln1ojung4rfd25mexi3b.bx.internal.cloudapp.net:8088/cluster"
 page=urllib.request.urlopen(webpage)
 soup=BeautifulSoup(page,'html.parser')
-# print(soup)
 
-# pattern = re.compile(r'.*id=(\d+).*.(\d\d:\d\d:\d\d).*')
+
+
 pattern=re.compile(r'appli[a-zA-Z]*\_\d+\_\d+')
 pattern1=re.compile(r'livy.*[a-zA-Z]*\_\d+')
 r1=re.findall(pattern1,soup.text)
@@ -24,7 +24,7 @@ application_id_list=list(unique_everseen(res))
 
 res1 = re.compile(r'\>appli[a-zA-Z]*\_\d+\_\d+.*livy.*[a-zA-Z]*\_\d+')
 result=re.findall(res1,soup.text)
-# print(result)
+
 dictLoader={}
 dictShaper={}
 dictRestate={}
@@ -42,45 +42,14 @@ print(dictShaper)
 print(dictRestate)
 
 
-# loaderList=[]
-# loaderAppIdList=[]
-# shaperList=[]
-# shaperAppIdList=[]
-# restateList=[]
-# restateAppIdList=[]
 
-
-# for w in r1:
-#     if(w.__contains__('Loader') and not(w.__contains__('Test' or 'test'))):
-#         loaderList.append(w[5:30])
-#     elif(w.__contains__('Shaper') and not(w.__contains__('Test' or 'test'))):
-#           shaperList.append(w[5:13])
-#     elif(w.__contains__('Restatement') and not(w.__contains__('Test' or 'test'))):
-#         restateList.append(w[5:37])
-#
-#
-# print(loaderList)
-# print(shaperList)
-# print(restateList)
-#
 from pandas import ExcelWriter
 
 outPath='C:\\Users\\apurva.sarode\\Documents\\Apurva\\temp_excel.xlsx'
 writer = ExcelWriter(outPath)
 
 
-# unused=["/application_1544591946163_8597","/application_1544591946163_8588","/application_1544591946163_8587","/application_1544591946163_8546","/application_1544591946163_8541","/application_1544591946163_8540","/application_1544591946163_8351","/application_1544591946163_8350","/application_1544591946163_8340","/application_1544591946163_8338","/application_1544591946163_8337","/application_1544591946163_8336","/application_1544591946163_8332","/application_1544591946163_8306","/application_1544591946163_8305","/application_1544591946163_8133","/application_1544591946163_7619","/application_1544591946163_7618","/application_1544591946163_8653","/application_1544591946163_8777","/application_1544591946163_8821","/application_1544591946163_8777","/application_1544591946163_8081","/application_1544591946163_8080","/application_1544591946163_8079","/application_1544591946163_8078",
-#         "/application_1544591946163_8891","/application_1544591946163_8890","/application_1544591946163_8889","/application_1544591946163_8888","/application_1544591946163_8887","/application_1544591946163_8886","/application_1544591946163_8885","/application_1544591946163_8884","/application_1544591946163_8734","/application_1544591946163_8733","/application_1544591946163_8732","/application_1544591946163_8731","/application_1544591946163_8730","/application_1544591946163_8729","/application_1544591946163_8728","/application_1544591946163_8727","/application_1544591946163_8667","/application_1544591946163_8138","/application_1544591946163_8134","/application_1544591946163_7617","/application_1544591946163_7616","/application_1544591946163_7615","/application_1544591946163_7614","/application_1544591946163_7368"]
 
-# temp_web="http://hn0-devrsi.ksccfrln1ojung4rfd25mexi3b.bx.internal.cloudapp.net:18080/history/application_1544591946163_8025/1/jobs/"
-# temp_page=urllib.request.urlopen(temp_web)
-# temp_soup=BeautifulSoup(temp_page,'html.parser')
-# temp_loader=re.sub('<(li)[^>]*>', r'<\1>', temp_soup.text, flags=re.I)
-# temp_res=re.sub('<(td)[^>]*>',r'<\1>',temp_loader,flags=re.I)
-# print(temp_loader)
-# if(temp_res.__contains__("2019/01/19")):
-#     b = temp_loader.rfind("Uptime")
-#     print(temp_res[b+20:b+30])
 
 loaderApplicationId=[]
 shaperApplicationId=[]
@@ -94,9 +63,7 @@ restateTime=[]
 tempList=['application_1544591946163_9087','application_1544591946163_9088','application_1544591946163_9087','application_1544591946163_9123']
 for app_id in application_id_list:
     if(app_id in dictLoader.keys()):
-        # print(app_id in dictLoader.keys())
         web1 = "http://hn0-devrsi.ksccfrln1ojung4rfd25mexi3b.bx.internal.cloudapp.net:18080/history/"+str(app_id)+"/1/jobs/"
-        # print("Loader+++++++++++++++++",app_id)
         page1=urllib.request.urlopen(web1)
         soup1=BeautifulSoup(page1,'html.parser')
         res1oader = re.sub('<(li)[^>]*>', r'<\1>', soup1.text, flags=re.I)
@@ -110,9 +77,7 @@ for app_id in application_id_list:
             df = pd.DataFrame({'Col_appId': loaderApplicationId,'Job_Name':loaderJob,'Time_Taken':loaderTime})
             df.to_excel(writer, 'Sheet1')
             writer.save()
-            # print("+++++++++++++++++++++++++++++++++++++++++++")
-            # print(df)
-
+            
 
     elif((app_id in dictShaper.keys()) and (app_id not in tempList)):
         web2 = "http://hn0-devrsi.ksccfrln1ojung4rfd25mexi3b.bx.internal.cloudapp.net:18080/history/" + str(app_id) + "/1/jobs/"
@@ -130,11 +95,11 @@ for app_id in application_id_list:
             df1 = pd.DataFrame({'Col_appId': shaperApplicationId,'Job_Name':shaperJob,'Time_Taken':shaperTime})
             df1.to_excel(writer, 'Sheet2')
             writer.save()
-            # print(df1)
+            
 
     elif(app_id in dictRestate.keys()):
         web3 = "http://hn0-devrsi.ksccfrln1ojung4rfd25mexi3b.bx.internal.cloudapp.net:18080/history/" + str(app_id) + "/1/jobs/"
-        # print("Resatement+++++++++++", app_id)
+        
         page3 = urllib.request.urlopen(web3)
         soup3 = BeautifulSoup(page3, 'html.parser')
         resrestate = re.sub('<(li)[^>]*>', r'<\1>', soup3.text, flags=re.I)
@@ -148,7 +113,7 @@ for app_id in application_id_list:
             df2 = pd.DataFrame({'Col_appId': restateApplicationId,'Job_Name':restateJob,'Time_Taken':restateTime})
             df2.to_excel(writer, 'Sheet3')
             writer.save()
-            # print(df2)
+            
 
 
 
